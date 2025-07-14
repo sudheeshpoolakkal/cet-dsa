987f83e776299352867fc7f768ff0305d384c39a
sudheesh <sudheeshpoolakkal@outlook.com>
2025-07-14 09:35:00 +0530

    modified linear search with functions

--- linear_search.c
+++ linear_search.c
@@ -1,31 +1,50 @@
-//without function
-#include<stdio.h>
-#include<time.h>
-int main(){
-    int n,i,found=0,key;
+// with function
+#include <stdio.h>
+#include <time.h>
+void input(int arr[]);
+void linear_search(int arr[]);
+int n;
+int main()
+{
     printf("Enter the number of elements in the array: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
+    input(arr);
+    linear_search(arr);
+    return 0;
+}
+
+void input(int arr[])
+{
+    int i;
+    
     printf("Enter the elements of the array: ");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
+}
+void linear_search(int arr[])
+{
+    int i, found = 0, key;
     printf("Enter an element to search: ");
-    scanf("%d",&key);
+    scanf("%d", &key);
     printf("Element found at positions: ");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        if(key==arr[i])
+        if (key == arr[i])
         {
             found++;
-            printf("%d ", i+1);
+            printf("%d ", i + 1);
         }
     }
-    if(found==0){
+    if (found == 0)
+    {
         printf("Element is not found in the array!\n");
-    } else{
-        printf("\nElement found %d times.\n",found);
     }
-    return 0;
+    else
+    {
+        printf("\nElement found %d times.\n", found);
+    }
+    
 }
