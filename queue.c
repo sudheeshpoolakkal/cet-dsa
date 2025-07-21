756f9c63fc05a12337b021f2764360f4506be5e6
sudheesh <sudheeshpoolakkal@outlook.com>
2025-07-21 10:48:36 +0530

    add isFull, isEmpty & peek functions to stack and queue

--- queue.c
+++ queue.c
@@ -4,7 +4,10 @@ int front =-1, rear=-1, max;
 void enqueue(int queue[]);
 void dequeue(int queue[]);
 void display(int queue[]);
-void main()
+int isFull();
+int isEmpty();
+void peek(int queue[]);
+int main()
 {
 
     printf("Enter the size of the queue: ");
@@ -14,20 +17,10 @@ void main()
     
     while (1)
     {
-        printf("1.Enqueue \n2.Dequeue\n3.Display\n4.Exit\n");
+        printf("\n1. Enqueue \n2. Dequeue\n3. Display\n4. Is Full?\n5. Is Empty?\n6. Peek\n7. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        if (choice == 4)
-        {
-            printf("Exiting the program, goodbye!\n");
-            free(queue);
-            break;
-        }
-        if (choice < 1 || choice > 4)
-        {
-            printf("Invalid choice. Please try again.\n");
-            continue;
-        }
+
 
         switch (choice)
         {
@@ -42,37 +35,47 @@ void main()
         case 3:
             display(queue);
             break;
+        case 4:
+            printf(isFull()?"Queue is Full!\n":"Queue is not full\n");
+            break;
+        case 5:
+            printf(isEmpty()?"Queue is empty!\n":"Queue is not empty\n");
+            break;
+        case 6:
+            peek(queue);
+            break;
+        case 7:
+            printf("Exiting the program, goodbye!\n");
+            free(queue);
+            exit(0);
 
         default:
             printf("Invalid choice! Please try again.\n");
         }
     }
+    return 0;
 }
 void enqueue(int queue[]){
-    if(rear==max-1){
+    if(isFull()){
         printf("Queue Overflow!\n");
-    }else{
+        return;
+    }
         int element;
+        printf("Enter a number to enqueue: ");
+        scanf("%d", &element);
         if(front==-1){
             front = 0;
             rear = 0;
-            printf("Enter a number to enqueue: ");
-            scanf("%d", &element);
-            queue[rear] = element;
-            printf("Element %d inserted to the queue!\n", element);
-        }
-        else{
+        }else{
             rear++;
-            printf("Enter a number to enqueue: ");  
-            scanf("%d", &element);
+        }
             queue[rear] = element;
             printf("Element %d inserted to the queue!\n", queue[rear]);
-        }
+
     }
-}
 
 void dequeue(int queue[]){
-    if(rear==-1){
+    if(isEmpty()){
         printf("Queue Underflow!\n");
     }else{
         printf("Element %d removed from the queue!\n", queue[front]);
@@ -85,7 +88,7 @@ void dequeue(int queue[]){
 }
 void display(int queue[])
 {
-    if (rear == -1)
+    if (isEmpty())
     {
         printf("Queue is empty!\n");
     }
@@ -97,5 +100,22 @@ void display(int queue[])
             printf("%d ", queue[i]);
         }
         printf("\n");
+    }
+}
+int isFull()
+{
+    return (rear == max - 1);
+}
+
+int isEmpty()
+{
+    return (rear == -1);
+}
+void peek(int queue[]){
+    if(isEmpty()){
+        printf("Queue is empty. nothing to peek!");
+    }
+    else{
+        printf("Front element: %d Rear element: %d",queue[front],queue[rear]);
     }
 }
--- stack.c
+++ stack.c
@@ -4,29 +4,21 @@ int top = -1, max;
 void push(int stack[]);
 void pop(int stack[]);
 void display(int stack[]);
-void main()
+int isFull();
+int isEmpty();
+void peek(int stack[]);
+int main()
 {
 
     printf("Enter the size of the stack: ");
     scanf("%d", &max);
     int *stack = (int *)malloc(max * sizeof(int));
     int choice;
-    printf("1.Push \n2.Pop\n3.Display\n4.Exit\n");
+    printf("1.Push \n2.Pop\n3.Display\n4.Is Full?\n5.Is Empty?\n6.peek\n7.Exit\n");
     while (1)
     {
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        if (choice == 4)
-        {
-            printf("Exiting the program, goodbye!\n");
-            free(stack);
-            break;
-        }
-        if (choice < 1 || choice > 4)
-        {
-            printf("Invalid choice. Please try again.\n");
-            continue;
-        }
 
         switch (choice)
         {
@@ -41,22 +33,35 @@ void main()
         case 3:
             display(stack);
             break;
+        case 4:
+            printf(isFull() ? "Stack is Full!\n" : "Stack is not full!\n");
+            break;
+        case 5:
+            printf(isEmpty() ? "Stack is Empty!\n" : "Stack is not empty!\n");
+            break;
+        case 6:
+            peek(stack);
+            break;
+        case 7:
+            free(stack);
+            exit(0);
 
         default:
             printf("Invalid choice! Please try again.\n");
         }
     }
+    return 0;
 }
 void push(int stack[])
 {
     int element;
-    if (top == max - 1)
+    if (isFull())
     {
-        printf("Stack Overflow!");
+        printf("Stack Overflow!\n");
     }
     else
     {
-        printf("Enter a number to push:");
+        printf("Enter a number to push: ");
         scanf("%d", &element);
         top++;
         stack[top] = element;
@@ -65,7 +70,7 @@ void push(int stack[])
 }
 void pop(int stack[])
 {
-    if (top == -1)
+    if (isEmpty())
     {
         printf("Stack Underflow!\n");
     }
@@ -89,5 +94,42 @@ void display(int stack[])
             printf("%d ", stack[i]);
         }
         printf("\n");
+    }
+}
+int isEmpty()
+{
+    
+    if (top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+int isFull()
+{
+    
+    if (top == max - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+void peek(int stack[])
+{
+    if (isEmpty())
+    {
+        printf("Stack is empty. Nothing to peek!\n");
+    }
+    else
+    {
+        printf("Top of the stack :%d\n", stack[top]);
     }
 }
